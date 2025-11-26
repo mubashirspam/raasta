c9ca8c75aa80511e43213c6ef871646e2f49cbb7
Mubashir Ahmed <mymac@Mubashirs-MacBook-Air.local>
2025-11-26 16:06:13 +0530

    bugfixed

--- next.config.ts
+++ next.config.ts
@@ -4,12 +4,13 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**",
       },
     ],
+    qualities: [75, 90, 100],
   },
 };
 
